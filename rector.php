57e096cde0dd414f40e6a4b40a45d3cd994f32e9
onyiimesi <onyedikachukwu62@gmail.com>
2025-11-18 10:56:05 +0100

    first commit

---
+++ rector.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodeQuality\Rector\FuncCall\CompactToVariablesRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\ValueObject\PhpVersion;
+use RectorLaravel\Set\LaravelLevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    // Paths to analyze
+    $rectorConfig->paths([
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ]);
+
+    // Skip specific rules
+    $rectorConfig->skip([
+        CompactToVariablesRector::class,
+    ]);
+
+    $rectorConfig->withSetProviders(LaravelSetProvider::class)
+        ->withComposerBased(laravel: true);
+
+    // Enable caching for Rector
+    $rectorConfig->cacheDirectory(__DIR__.'/storage/rector');
+    $rectorConfig->cacheClass(FileCacheStorage::class);
+
+    // Apply sets for Laravel and general code quality
+    $rectorConfig->sets([
+        LaravelLevelSetList::UP_TO_LARAVEL_110,
+        LaravelLevelSetList::CODE_QUALITY,
+        LaravelLevelSetList::EARLY_RETURN,
+        LaravelLevelSetList::TYPE_DECLARATION,
+        LaravelLevelSetList::DEAD_CODE,
+        LaravelLevelSetList::LARAVEL_COLLECTION,
+        LaravelLevelSetList::LARAVEL_STATIC_TO_INJECTION,
+        LaravelLevelSetList::LARAVEL_TESTING,
+    ]);
+
+    // Define PHP version for Rector
+    $rectorConfig->phpVersion(PhpVersion::PHP_83);
+};

5982e60bf216d092f6d73a9103376d63b6cb5721
onyiimesi <onyedikachukwu62@gmail.com>
2025-12-03 14:00:01 +0100

    a

--- app/Actions/CreateUserAction.php
+++ app/Actions/CreateUserAction.php
@@ -15,19 +15,23 @@ final readonly class CreateUserAction
      */
     public function handle(CreateUserDto $dto): User
     {
-        return User::create([
-            'first_name' => $dto->request->string('first_name'),
-            'last_name' => $dto->request->string('last_name'),
-            'email' => $dto->request->string('email'),
-            'email_verified_at' => $dto->request->string('email_verified_at') ?? null,
-            'is_verified' => $dto->request->boolean('is_verified') ?? false,
-            'type' => $dto->request->string('type'),
-            'country_id' => $dto->request->integer('country_id'),
-            'state_id' => $dto->request->integer('state_id'),
-            'default_currency' => $dto->currencyCode,
-            'password' => bcrypt($dto->request->input('password')),
-            'signed_up_from' => $dto->request->string('signed_up_from'),
-            'status' => $dto->request->enum('status', UserStatus::class) ?? UserStatus::PENDING,
-        ]);
+        return User::updateOrCreate(
+            [
+                'email' => $dto->request->input('email'),
+            ],
+            [
+                'first_name' => $dto->request->string('first_name'),
+                'last_name' => $dto->request->string('last_name'),
+                'email_verified_at' => $dto->request->string('email_verified_at') ?? null,
+                'is_verified' => $dto->request->boolean('is_verified') ?? false,
+                'type' => $dto->request->string('type'),
+                'country_id' => $dto->request->integer('country_id'),
+                'state_id' => $dto->request->integer('state_id'),
+                'default_currency' => $dto->currencyCode,
+                'password' => bcrypt($dto->request->input('password')),
+                'signed_up_from' => $dto->request->string('signed_up_from'),
+                'status' => $dto->request->enum('status', UserStatus::class) ?? UserStatus::PENDING,
+            ]
+        );
     }
 }

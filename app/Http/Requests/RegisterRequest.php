f5d88cbf22f021f872d5c84d4736d72bd7a5803e
onyiimesi <onyedikachukwu62@gmail.com>
2026-01-20 10:54:46 +0100

    Added disposeable email restruction on prod

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -6,12 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\Rule;
 use App\Enum\UserType;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Str;
 
 class RegisterRequest extends FormRequest
 {
     public function rules(): array
     {
-        return [
+        $rules = [
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255'],
@@ -21,5 +23,19 @@ class RegisterRequest extends FormRequest
             'password' => ['required', 'string', Password::defaults()],
             'signed_up_from' => ['required', 'string'],
         ];
+
+        if (App::environment('production')) {
+            $rules['email'][] = function ($attribute, $value, $fail) {
+                $blockedDomains = config('disposableemail.domains', []);
+
+                $domain = strtolower(trim(Str::after($value, '@')));
+
+                if (in_array($domain, $blockedDomains, true)) {
+                    $fail('Disposable or test email addresses are not allowed.');
+                }
+            };
+        }
+
+        return $rules;
     }
 }
